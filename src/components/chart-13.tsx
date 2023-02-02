3b364ba28c8ba76eb0d20b534908b2c466515f38
HXL1111 <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-02 17:00:07 +0800

    完成chart 13

--- src/components/chart-13.tsx
+++ src/components/chart-13.tsx
@@ -2,14 +2,25 @@ import React, { useEffect, useRef } from 'react'
 import * as echarts from 'echarts'
 import { createEchartsOptions } from '../shared/create-echarts-options'
 
-export const Chart1 = () => {
+export const Chart13 = () => {
   const divRef = useRef(null)
+  const data = [
+    { value: 0.08, name: '东岗路' },
+    { value: 0.06, name: '段家滩' },
+    { value: 0.11, name: '雁北' },
+    { value: 0.09, name: '五泉山' },
+    { value: 0.12, name: '中山路' },
+    { value: 0.06, name: '庆阳路' },
+    { value: 0.08, name: '武都路' },
+    { value: 0.08, name: '酒泉路' },
+    { value: 0.08, name: '天水路' },
+  ]
   useEffect(() => {
     var myChart = echarts.init(divRef.current)
     myChart.setOption(
       createEchartsOptions({
         xAxis: {
-          data: ['关城区', '七里河区', '西固区', '安宁区', '红古区', '永登区', '皋兰区', '榆中区', '兰州新区'],
+          data: data.map((i) => i.name),
           axisTick: { show: false },
           axisLine: {
             lineStyle: { color: '#083B70' },
@@ -26,26 +37,38 @@ export const Chart1 = () => {
             },
           },
         },
+
         yAxis: {
           splitLine: { show: false },
           axisLine: {
             show: true,
             lineStyle: { color: '#083B70' },
           },
+          axisLabel: {
+            formatter(value) {
+              return (value * 100).toFixed(0) + '%'
+            },
+          },
         },
         series: [
           {
             type: 'bar',
-            data: [10, 20, 36, 41, 15, 26, 37, 18, 29],
+            data: data.map((i) => i.value),
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: '#0A97FB',
+              },
+              {
+                offset: 1,
+                color: '#1E34FA',
+              },
+            ]),
           },
         ],
       })
     )
-  })
-  return (
-    <div className="bordered 管辖统计">
-      <h2>案发派出所管辖统计</h2>
-      <div ref={divRef} className="chart"></div>
-    </div>
-  )
+  }, [])
+
+  return <div ref={divRef} className="chart"></div>
 }
